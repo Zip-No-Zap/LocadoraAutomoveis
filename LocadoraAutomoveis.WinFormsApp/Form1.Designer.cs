d45208e59064f2040eb4d24b79c1182b3b8717ca
William Ludwig <acesso.william@gmail.com>
2022-06-22 10:22:13 -0300

    Criado projeto MSTest Testes Unitarios: Locadora

--- LocadoraAutomoveis.WinFormsApp/Form1.Designer.cs
+++ LocadoraAutomoveis.WinFormsApp/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(837, 450);
+            this.ClientSize = new System.Drawing.Size(994, 529);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
