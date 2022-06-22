1694fc1ccbc9882a9f041a63362b0ec70045f978
LUIS HENRIQUE FERREIRA KRAUS <luishenriquekraus@hotmail.com>
2022-06-22 13:46:37 -0300

    Dominio Funcionario Criado

---
+++ LocadoraVeiculos.Dominio/Modulo Funcionario/ValidadorFuncionario.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraVeiculos.Dominio.Modulo_Funcionario
+{
+    public class ValidadorFuncionario : AbstractValidator<Funcionario>
+    {
+        public ValidadorFuncionario()
+        {
+
+        }
+    }
+}
