7f5f7bf4c605835b8da422ff4faad38775074f30
William Ludwig <acesso.william@gmail.com>
2022-06-22 11:50:54 -0300

    Adicionado classes mae cliente: Locadora

--- LocadoraVeiculos.Dominio/Modulo Cliente/Cliente.cs
+++ LocadoraVeiculos.Dominio/Modulo Cliente/Cliente.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LocadoraVeiculos.Dominio.Compartilhado;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace LocadoraVeiculos.Dominio.Modulo_Cliente
 {
-    public class Cliente : EntidadeBase<Cliente>
+    public class Cliente<T> where T : EntidadeBase<Pessoa>
     {
+        //public Cliente()
+        //{
+        //    var cliente01 = new Cliente<PessoaFisica>();
+        //}
     }
 }
