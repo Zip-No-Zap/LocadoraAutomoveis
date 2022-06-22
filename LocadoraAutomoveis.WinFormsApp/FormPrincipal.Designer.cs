ca7b7f38cf06f9d7bb8f650e7285e47dc1346f57
William Ludwig <acesso.william@gmail.com>
2022-06-22 11:53:18 -0300

    Adicionado menuStrip no FormPrincial: Locadora

--- LocadoraAutomoveis.WinFormsApp/FormPrincipal.Designer.cs
+++ LocadoraAutomoveis.WinFormsApp/FormPrincipal.Designer.cs
@@ -28,23 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuPrincipal = new System.Windows.Forms.MenuStrip();
+            this.cadastroToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clienteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.funcionarioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.grupoDeVeículoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.taxaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuPrincipal.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuPrincipal
+            // 
+            this.menuPrincipal.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuPrincipal.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cadastroToolStripMenuItem});
+            this.menuPrincipal.Location = new System.Drawing.Point(0, 0);
+            this.menuPrincipal.Name = "menuPrincipal";
+            this.menuPrincipal.Size = new System.Drawing.Size(1342, 33);
+            this.menuPrincipal.TabIndex = 0;
+            this.menuPrincipal.Text = "menuStrip1";
+            // 
+            // cadastroToolStripMenuItem
+            // 
+            this.cadastroToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clienteToolStripMenuItem,
+            this.funcionarioToolStripMenuItem,
+            this.grupoDeVeículoToolStripMenuItem,
+            this.taxaToolStripMenuItem});
+            this.cadastroToolStripMenuItem.Name = "cadastroToolStripMenuItem";
+            this.cadastroToolStripMenuItem.Size = new System.Drawing.Size(99, 29);
+            this.cadastroToolStripMenuItem.Text = "Cadastro";
+            // 
+            // clienteToolStripMenuItem
+            // 
+            this.clienteToolStripMenuItem.Name = "clienteToolStripMenuItem";
+            this.clienteToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.clienteToolStripMenuItem.Text = "Cliente";
+            // 
+            // funcionarioToolStripMenuItem
+            // 
+            this.funcionarioToolStripMenuItem.Name = "funcionarioToolStripMenuItem";
+            this.funcionarioToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.funcionarioToolStripMenuItem.Text = "Funcionario";
+            // 
+            // grupoDeVeículoToolStripMenuItem
+            // 
+            this.grupoDeVeículoToolStripMenuItem.Name = "grupoDeVeículoToolStripMenuItem";
+            this.grupoDeVeículoToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.grupoDeVeículoToolStripMenuItem.Text = "Grupo de Veículo";
+            // 
+            // taxaToolStripMenuItem
+            // 
+            this.taxaToolStripMenuItem.Name = "taxaToolStripMenuItem";
+            this.taxaToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.taxaToolStripMenuItem.Text = "Taxa";
             // 
             // FormPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1342, 688);
+            this.Controls.Add(this.menuPrincipal);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuPrincipal;
             this.MaximizeBox = false;
             this.Name = "FormPrincipal";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Locadora de Veículos 1.0";
+            this.menuPrincipal.ResumeLayout(false);
+            this.menuPrincipal.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuPrincipal;
+        private System.Windows.Forms.ToolStripMenuItem cadastroToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clienteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem funcionarioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem grupoDeVeículoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem taxaToolStripMenuItem;
     }
 }
