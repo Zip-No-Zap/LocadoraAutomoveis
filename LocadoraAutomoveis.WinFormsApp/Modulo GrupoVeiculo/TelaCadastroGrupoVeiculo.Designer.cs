e5966ef5715273d41c42759acbb929414d5d2ecf
William Ludwig <acesso.william@gmail.com>
2022-06-24 08:39:06 -0300

    labels janela grupo veiculo

--- LocadoraAutomoveis.WinFormsApp/Modulo GrupoVeiculo/TelaCadastroGrupoVeiculo.Designer.cs
+++ LocadoraAutomoveis.WinFormsApp/Modulo GrupoVeiculo/TelaCadastroGrupoVeiculo.Designer.cs
@@ -32,78 +32,69 @@
             this.btnLimpar = new System.Windows.Forms.Button();
             this.btnOK = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.tbListaDeCarros = new System.Windows.Forms.TextBox();
             this.label8 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.tbNomeGrupoVeiculo = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLight;
             this.panel1.Controls.Add(this.btnLimpar);
             this.panel1.Controls.Add(this.btnOK);
-            this.panel1.Location = new System.Drawing.Point(-1, 139);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Location = new System.Drawing.Point(3, 128);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(412, 60);
+            this.panel1.Size = new System.Drawing.Size(580, 100);
             this.panel1.TabIndex = 0;
             // 
             // btnLimpar
             // 
-            this.btnLimpar.Location = new System.Drawing.Point(94, 8);
-            this.btnLimpar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnLimpar.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnLimpar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLimpar.Location = new System.Drawing.Point(130, 24);
             this.btnLimpar.Name = "btnLimpar";
-            this.btnLimpar.Size = new System.Drawing.Size(78, 35);
+            this.btnLimpar.Size = new System.Drawing.Size(111, 58);
             this.btnLimpar.TabIndex = 2;
             this.btnLimpar.Text = "Limpar";
-            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.UseVisualStyleBackColor = false;
             this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
             // 
             // btnOK
             // 
+            this.btnOK.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnOK.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.btnOK.Location = new System.Drawing.Point(12, 8);
-            this.btnOK.Margin = new System.Windows.Forms.Padding(2);
+            this.btnOK.Location = new System.Drawing.Point(13, 24);
             this.btnOK.Name = "btnOK";
-            this.btnOK.Size = new System.Drawing.Size(78, 35);
+            this.btnOK.Size = new System.Drawing.Size(111, 58);
             this.btnOK.TabIndex = 1;
             this.btnOK.Text = "OK";
-            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.UseVisualStyleBackColor = false;
             this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
             // 
             // panel2
             // 
             this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel2.Controls.Add(this.tbListaDeCarros);
+            this.panel2.Controls.Add(this.comboBox1);
             this.panel2.Controls.Add(this.panel1);
             this.panel2.Controls.Add(this.label8);
             this.panel2.Controls.Add(this.label1);
             this.panel2.Controls.Add(this.tbNomeGrupoVeiculo);
-            this.panel2.Location = new System.Drawing.Point(8, 7);
-            this.panel2.Margin = new System.Windows.Forms.Padding(2);
+            this.panel2.Location = new System.Drawing.Point(11, 12);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(412, 200);
+            this.panel2.Size = new System.Drawing.Size(588, 235);
             this.panel2.TabIndex = 1;
-            // 
-            // tbListaDeCarros
-            // 
-            this.tbListaDeCarros.Location = new System.Drawing.Point(195, 19);
-            this.tbListaDeCarros.Margin = new System.Windows.Forms.Padding(2);
-            this.tbListaDeCarros.Name = "tbListaDeCarros";
-            this.tbListaDeCarros.Size = new System.Drawing.Size(188, 23);
-            this.tbListaDeCarros.TabIndex = 18;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label8.Location = new System.Drawing.Point(195, 4);
-            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Location = new System.Drawing.Point(279, 28);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(82, 13);
+            this.label8.Size = new System.Drawing.Size(113, 21);
             this.label8.TabIndex = 17;
             this.label8.Text = "Lista de Carros";
             // 
@@ -111,36 +102,42 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(11, 4);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(16, 28);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.Size = new System.Drawing.Size(123, 21);
             this.label1.TabIndex = 7;
             this.label1.Text = "Nome do Grupo";
             // 
             // tbNomeGrupoVeiculo
             // 
-            this.tbNomeGrupoVeiculo.Location = new System.Drawing.Point(11, 19);
-            this.tbNomeGrupoVeiculo.Margin = new System.Windows.Forms.Padding(2);
+            this.tbNomeGrupoVeiculo.Location = new System.Drawing.Point(16, 54);
             this.tbNomeGrupoVeiculo.Name = "tbNomeGrupoVeiculo";
-            this.tbNomeGrupoVeiculo.Size = new System.Drawing.Size(160, 23);
+            this.tbNomeGrupoVeiculo.Size = new System.Drawing.Size(227, 31);
             this.tbNomeGrupoVeiculo.TabIndex = 0;
             this.tbNomeGrupoVeiculo.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbNome_KeyPress);
             this.tbNomeGrupoVeiculo.Leave += new System.EventHandler(this.tbNome_Leave);
             // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(279, 52);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(286, 33);
+            this.comboBox1.TabIndex = 19;
+            // 
             // TelaCadastroGrupoVeiculo
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(430, 218);
+            this.ClientSize = new System.Drawing.Size(614, 261);
             this.Controls.Add(this.panel2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
             this.Name = "TelaCadastroGrupoVeiculo";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Cadastro de Grupo Veiculo";
+            this.Text = "Cadastro de Grupo Veículo";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.TelaCadastroGrupoVeiculo_FormClosing);
             this.Load += new System.EventHandler(this.TelaCadastroGrupoVeiculo_Load);
             this.panel1.ResumeLayout(false);
@@ -159,6 +156,6 @@
         private System.Windows.Forms.TextBox tbNomeGrupoVeiculo;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.TextBox tbListaDeCarros;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
