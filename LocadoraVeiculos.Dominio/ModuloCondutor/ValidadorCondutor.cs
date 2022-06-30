7e6c265c045ead39868fc2ea0b85fdec2b87eba9
Ana Beatriz Oliveira <ana.bo22@aluno.ifsc.edu.br>
2022-06-30 16:51:56 -0300

    Implementado o ValidadorCondutor.

--- LocadoraVeiculos.Dominio/ModuloCondutor/ValidadorCondutor.cs
+++ LocadoraVeiculos.Dominio/ModuloCondutor/ValidadorCondutor.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using FluentValidation;
+using FluentValidation.Validators;
+using LocadoraVeiculos.Dominio.Compartilhado;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,37 @@ using System.Threading.Tasks;
 
 namespace LocadoraVeiculos.Dominio.ModuloCondutor
 {
-    public class ValidadorCondutor
+    public class ValidadorCondutor : ValidadorBase<Condutor>
     {
+        public ValidadorCondutor()
+        {
+            RuleFor(x => x.Nome)
+                .NotNull().WithMessage("Campo 'Nome' é obrigatório.")
+                .NotEmpty().WithMessage("Campo 'Nome' é obrigatótio.");
+
+            RuleFor(x => x.Cpf)
+             .Cpf();
+
+            RuleFor(x => x.Endereco)
+              .NotNull().WithMessage("Campo 'Endereço' é obrigatório.")
+              .NotEmpty().WithMessage("Campo 'Endereço' é obrigatório.");
+
+            RuleFor(x => x.Email)
+             .EmailAddress(EmailValidationMode.AspNetCoreCompatible).WithMessage("'Email' com formato incorreto")
+             .NotNull().NotEmpty().WithMessage("'Email' com formato incorreto");
+
+            RuleFor(x => x.Telefone)
+               .Telefone()
+               .NotEqual("(  )      -");
+
+            RuleFor(x => x.Cnh)
+                .NotNull().NotEmpty()
+                .MaximumLength(12)
+                .WithMessage("'CNH' inválido.");
+
+            RuleFor(x => x.VencimentoCnh)
+                .NotEqual(DateTime.MinValue)
+                .WithMessage("O campo 'Vencimento CNH' é obrigatório");
+        }
     }
 }
