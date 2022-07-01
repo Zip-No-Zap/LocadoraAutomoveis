fdb34d33e9c27dbdbddcff263965ae37f9822a89
Ana Beatriz Oliveira <ana.bo22@aluno.ifsc.edu.br>
2022-07-01 11:47:28 -0300

    Criado Mapeador, Repositorio e Script do Condutor

---
+++ LocadoraVeiculos.Infra/Modulo Condutor/MapeadorCondutor.cs
@@ -0,0 +1,34 @@
+﻿using LocadoraVeiculos.Dominio.Modulo_Condutor;
+using LocadoraVeiculos.Infra.BancoDados.Compartilhado;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraVeiculos.Infra.BancoDados.Modulo_Condutor
+{
+    public class MapeadorCondutor : MapeadorBase<Condutor>
+    {
+        public override void DefinirParametros(Condutor entidade, SqlCommand cmd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void DefinirParametroValidacao(string campoBd, Condutor entidade, SqlCommand cmd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<Condutor> LerTodos(SqlDataReader leitor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Condutor LerUnico(SqlDataReader leitor)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LocadoraVeiculos.Infra/Modulo Condutor/RepositorioCondutorEmBancoDados.cs
@@ -0,0 +1,28 @@
+﻿using LocadoraVeiculos.Dominio.Modulo_Condutor;
+using LocadoraVeiculos.Infra.BancoDados.Compartilhado;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraVeiculos.Infra.BancoDados.Modulo_Condutor
+{
+    public class RepositorioCondutorEmBancoDados : RepositorioBase<Condutor, MapeadorCondutor, ValidadorCondutor>
+    {
+        protected override string Sql_insercao => throw new NotImplementedException();
+
+        protected override string Sql_edicao => throw new NotImplementedException();
+
+        protected override string Sql_exclusao => throw new NotImplementedException();
+
+        protected override string Sql_selecao_por_id => throw new NotImplementedException();
+
+        protected override string Sql_selecao_todos => throw new NotImplementedException();
+
+        protected override bool VerificarDuplicidade(Condutor entidade)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
