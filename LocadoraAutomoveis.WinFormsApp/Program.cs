8c438ce245169c031dfcb06ebdb982268e6f7b3f
William Ludwig <acesso.william@gmail.com>
2022-07-06 13:24:44 -0300

    alterado program para serilo

--- LocadoraAutomoveis.WinFormsApp/Program.cs
+++ LocadoraAutomoveis.WinFormsApp/Program.cs
@@ -1,3 +1,7 @@
+using Cqrs.Hosts;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,12 +16,24 @@ namespace LocadoraAutomoveis.WinFormsApp
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+
+            CreateHostbuilder(args).Build().Run();
+
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FormPrincipal());
         }
+
+        public static IHostBuilder CreateHostbuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args).UseSerilog().ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<StartUp>();
+            }
+            );
+
     }
 }
