b178babd6c736c7acda9b1b6a1e7533f2cbcf0fc
William Ludwig <acesso.william@gmail.com>
2022-07-06 15:11:44 -0300

    logger criado

---
+++ LocadoraAutomoveis.Infra.Logs/ConfiguracaoLogger.cs
@@ -0,0 +1,16 @@
+﻿using Serilog;
+
+
+namespace LocadoraAutomoveis.Infra.Logs
+{
+    public static class ConfiguracaoLogger
+    {
+        public static void CriarLogger()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()   
+                .WriteTo.File(@"C:\temp\Logs\Log.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+        }
+    }
+}
--- LocadoraAutomoveis.WinFormsApp/Program.cs
+++ LocadoraAutomoveis.WinFormsApp/Program.cs
@@ -1,11 +1,5 @@
-using Cqrs.Hosts;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
-using Serilog;
+using LocadoraAutomoveis.Infra.Logs;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace LocadoraAutomoveis.WinFormsApp
@@ -18,22 +12,12 @@ namespace LocadoraAutomoveis.WinFormsApp
         [STAThread]
         static void Main(string[] args)
         {
-            Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
-
-            CreateHostbuilder(args).Build().Run();
+            ConfiguracaoLogger.CriarLogger();
 
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FormPrincipal());
         }
-
-        public static IHostBuilder CreateHostbuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args).UseSerilog().ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<StartUp>();
-            }
-            );
-
     }
 }
