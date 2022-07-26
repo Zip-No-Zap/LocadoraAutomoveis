07ec7f96f95f409d17580ee826db97d73b540682
Ana Beatriz Oliveira <ana.bo22@aluno.ifsc.edu.br>
2022-07-26 16:24:29 -0300

    Atualizando mapeador cliente e condutor.

--- LocadoraAutomoveis.Infra.Orm/ModuloCliente/MapeadorClienteOrm.cs
+++ LocadoraAutomoveis.Infra.Orm/ModuloCliente/MapeadorClienteOrm.cs
@@ -8,6 +8,15 @@ namespace LocadoraAutomoveis.Infra.Orm.ModuloCliente
     {
         public void Configure(EntityTypeBuilder<Cliente> builder)
         {
+            builder.ToTable("TBCLIENTE");
+            builder.Property(x => x.Id).ValueGeneratedNever();
+            builder.Property(x => x.Nome).HasColumnType("varchar(100)").IsRequired();
+            builder.Property(x => x.Email).HasColumnType("varchar(100)").IsRequired();
+            builder.Property(x => x.Telefone).HasColumnType("varchar(20)").IsRequired();
+            builder.Property(x => x.Endereco).HasColumnType("varchar(100)").IsRequired();
+            builder.Property(x => x.TipoCliente).HasColumnType("varchar(10)").IsRequired();
+            builder.Property(x => x.Cpf).HasColumnType("varchar(20)");
+            builder.Property(x => x.Cnpj).HasColumnType("varchar(20)");
 
         }
     }
--- LocadoraAutomoveis.Infra.Orm/ModuloCondutor/MapeadorCondutorOrm.cs
+++ LocadoraAutomoveis.Infra.Orm/ModuloCondutor/MapeadorCondutorOrm.cs
@@ -8,6 +8,20 @@ namespace LocadoraAutomoveis.Infra.Orm.ModuloCondutor
     {
         public void Configure(EntityTypeBuilder<Condutor> builder)
         {
+            builder.ToTable("TBCONDUTOR");
+            builder.Property(x => x.Id).ValueGeneratedNever();
+            builder.Property(x => x.Nome).HasColumnType("varchar(100)").IsRequired();
+            builder.Property(x => x.Email).HasColumnType("varchar(100)").IsRequired();
+            builder.Property(x => x.Telefone).HasColumnType("varchar(20)").IsRequired();
+            builder.Property(x => x.Endereco).HasColumnType("varchar(100)").IsRequired();
+            builder.Property(x => x.Cpf).HasColumnType("varchar(20)").IsRequired();
+            builder.Property(x => x.Cnh).HasColumnType("varchar(20)").IsRequired();
+            builder.Property(x => x.VencimentoCnh).HasColumnType("date").IsRequired();
+
+            builder.HasOne(x => x.Cliente);
+
+
+
         }
     }
 }
