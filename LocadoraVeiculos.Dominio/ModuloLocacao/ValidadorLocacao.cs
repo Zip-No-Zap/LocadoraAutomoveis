cb0b0b949fbc4b017d6953e16eaf904aba1144f8
William Ludwig <acesso.william@gmail.com>
2022-07-31 14:22:18 -0300

    validações

--- LocadoraVeiculos.Dominio/ModuloLocacao/ValidadorLocacao.cs
+++ LocadoraVeiculos.Dominio/ModuloLocacao/ValidadorLocacao.cs
@@ -7,15 +7,17 @@ namespace LocadoraVeiculos.Dominio.ModuloLocacao
 {
     public class ValidadorLocacao : ValidadorBase<Locacao>
     {
-        public ValidadorLocacao()
+         public ValidadorLocacao()
         {
-            RuleFor(x => x.CondutorLocacao).NotEmpty().WithMessage("Campo 'Cliente', é obrigatório");
+            RuleFor(x => x.ClienteLocacao).NotEmpty().WithMessage("Campo 'Cliente', é obrigatório");
+            RuleFor(x => x.CondutorLocacao).NotEmpty().WithMessage("Campo 'Condutor', é obrigatório");
+            RuleFor(x => x.Grupo).NotEmpty().WithMessage("Campo 'Grupo', é obrigatório");
             RuleFor(x => x.VeiculoLocacao).NotEmpty().WithMessage("Campo 'Veículo', é obrigatório");
-            // RuleFor(x => x.PlanoLocacao).NotEmpty().WithMessage("Campo 'Plano', é obrigatório");
+            RuleFor(x => x.PlanoLocacao).NotEmpty().WithMessage("Campo 'Plano', é obrigatório");
 
             RuleFor(x => x.DataLocacao)
-                .NotEmpty().WithMessage("Campo 'Data Locação', é obrigatório");
-              //  .GreaterThan(DateTime.Today).WithMessage("'Data de locação' deve ser maior que hoje");
+                .NotEmpty().WithMessage("Campo 'Data Locação', é obrigatório")
+                .GreaterThan(DateTime.Today.Subtract(TimeSpan.FromDays(1))).WithMessage("'Data de locação' deve ser hoje ou futura");
 
             RuleFor(x => x.DataDevolucao)
                 .NotEmpty().WithMessage("Campo 'Data Devolução', é obrigatório")
